72ffce9c287aca620c94a2bfbfcb18b26840ad3a
saishkulkarni <saishkulkarni7@gmail.com>
2023-04-24 14:46:21 +0530

    updated

--- src/main/java/org/jsp/super_market/controller/AdminController.java
+++ src/main/java/org/jsp/super_market/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.jsp.super_market.helper.Login;
 import org.jsp.super_market.helper.ResponseStructure;
 import org.jsp.super_market.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,26 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("admin")
+@CrossOrigin
 public class AdminController {
 
 	@Autowired
 	AdminService service;
 
 	@PostMapping("login")
-	public ResponseStructure<Login> login(@RequestBody Login login) throws AllException
-	{
+	public ResponseStructure<Login> login(@RequestBody Login login) throws AllException {
 		return service.login(login);
 	}
-	
+
 	@GetMapping("product/fetch")
-	public ResponseStructure<List<Product>> fetchProducts()
-	{
+	public ResponseStructure<List<Product>> fetchProducts() {
 		return service.fetchProducts();
 	}
-	
+
 	@PutMapping("product/status/{pid}")
-	public ResponseStructure<Product> changeStatus(@PathVariable int pid)
-	{
+	public ResponseStructure<Product> changeStatus(@PathVariable int pid) {
 		return service.changeStatus(pid);
 	}
 }
